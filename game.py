335db7fb56166e087b6073c1f6a4e3b767d0827e
rotiv2809 <vitorpzie@gmail.com>
2024-06-12 11:55:44 -0300

    Gravidade_implementada

--- game.py
+++ game.py
@@ -4,21 +4,54 @@ import sys
 
 pygame.init()
 
-screen_width = 600
-screen_height = 800
+screen_width = 1080
+screen_height = 720
 WHITE = (255, 255, 255)
 
 pygame.display.set_caption("Street Fighter")
 screen = pygame.display.set_mode((screen_width, screen_height))
 
-ryu = pygame.image.load(os.path.join('imgs', 'minha_imagem.jpeg'))
 
-# Define um retângulo de sprite para ele
-ryu_rect = ryu.get_rect()
-ryu_rect.topleft = (100, screen_height - 150) 
+class Hadouken:
+    def __init__(self, Character):
+        self.imagem = pygame.image.load(os.path.join('imgs','hadouken.jpg'))
+        self.x = Character.x
+        self.y = Character.y
+        self.speed = 0.7
+    def mover(self):
+        self.x += self.speed
+    
 
-speed = 1 
-
+class Character:
+    def __init__(self, x, y): 
+        self.imagem = pygame.image.load(os.path.join('imgs', 'minha_imagem.png'))
+        self.rect = self.imagem.get_rect()
+        self.x = x
+        self.y = y
+        self.speed_y = 0
+        self.on_ground = True
+        
+    def pular(self):
+        # tenho que adicionar gravidade e fazer a animação de mortal do pulo
+        if(self.on_ground):
+            self.speed_y -= 5
+            self.on_ground = False
+        
+    def gravidade(self):
+        if not self.on_ground:
+            self.speed_y += 0.04
+    
+    def mover(self):
+        self.rect.y += self.speed_y
+        if self.rect.y >= screen_height - 50 - self.rect.height:
+            self.rect.y = screen_height - 50 - self.rect.height
+            self.speed_y = 0
+            self.on_ground = True
+    
+    def hadouken(self):
+        hadouken_gerado = Hadouken(0,self)
+        hadouken_gerado.x += speed
+        
 class Ground:
     def __init__(self, x, y, width, height):
         self.rect = pygame.Rect(x, y, width, height)
@@ -30,34 +63,55 @@ class Ground:
 
 ground = Ground(0, screen_height - 50, screen_width, 50)
 
+ryu = Character(100, 100)
+
+# Define um retângulo de sprite para ele
+ryu.rect.topleft = (100, screen_height - 150) 
+
+#background
+background = pygame.image.load(os.path.join('imgs','background.jpeg'))
+background = pygame.transform.scale(background, (screen_width, screen_height))
+background_rect = background.get_rect()
+background_rect.topleft = (0,0)
+
+speed = 1 
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
 
+    
+    
     keys = pygame.key.get_pressed()
-
+    
     if keys[pygame.K_LEFT]:
-        ryu_rect.x -= speed
+        ryu.rect.x -= speed
     if keys[pygame.K_RIGHT]:
-        ryu_rect.x += speed
+        ryu.rect.x += speed
     if keys[pygame.K_UP]:
-        ryu_rect.y -= speed
+        ryu.pular()
     if keys[pygame.K_DOWN]:
-        ryu_rect.y += speed
+        ryu.rect.y += speed
+    
+    ryu.gravidade()    
+    ryu.mover()
+    
+    #limites da tela
 
-    if ryu_rect.x < 0:
-        ryu_rect.x = 0
-    if ryu_rect.x > screen_width - ryu_rect.width:
-        ryu_rect.x = screen_width - ryu_rect.width
-    if ryu_rect.y < 0:
-        ryu_rect.y = 0
-    if ryu_rect.y > screen_height - ground.rect.height - ryu_rect.height:
-        ryu_rect.y = screen_height - ground.rect.height - ryu_rect.height
+    if ryu.rect.x < 0:
+        ryu.rect.x = 0
+    if ryu.rect.x > screen_width - ryu.rect.width:
+        ryu.rect.x = screen_width - ryu.rect.width
+    if ryu.rect.y < 0:
+        ryu.rect.y = 0
+    if ryu.rect.y > screen_height - ground.rect.height - ryu.rect.height:
+        ryu.rect.y = screen_height - ground.rect.height - ryu.rect.height
 
     screen.fill(WHITE)
     ground.draw(screen)  # Desenha o chão
-    screen.blit(ryu, ryu_rect.topleft)  # Desenha a imagem na nova posição
+    screen.blit(background, background_rect.topleft)
+    screen.blit(ryu.imagem, ryu.rect.topleft)  # Desenha a imagem na nova posição
     pygame.display.flip()
 
