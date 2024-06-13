9238c9db529dadfc8be9b56bb90c4762761df31b
rotiv2809 <vitorpzie@gmail.com>
2024-06-13 11:04:31 -0300

    oi

--- game.py
+++ game.py
@@ -7,7 +7,7 @@ pygame.init()
 screen_width = 1080
 screen_height = 720
 WHITE = (255, 255, 255)
-IMAGENS_PARADO = [
+IMAGENS_PARA_TRAS = [
     pygame.transform.scale2x(pygame.image.load(os.path.join('imgs/parado', 'imagem_1.png'))),
     pygame.transform.scale2x(pygame.image.load(os.path.join('imgs/parado', 'imagem_2.png'))),
     pygame.transform.scale2x(pygame.image.load(os.path.join('imgs/parado', 'imagem_3.png'))),
@@ -15,6 +15,15 @@ IMAGENS_PARADO = [
     pygame.transform.scale2x(pygame.image.load(os.path.join('imgs/parado', 'imagem_5.png'))),
     pygame.transform.scale2x(pygame.image.load(os.path.join('imgs/parado', 'imagem_6.png'))),
 ]
+IMAGENS_PARA_FRENTE = [
+    pygame.transform.scale2x(pygame.image.load(os.path.join('imgs/para_frente', 'imagem_1.png'))),
+    pygame.transform.scale2x(pygame.image.load(os.path.join('imgs/para_frente', 'imagem_2.png'))),
+    pygame.transform.scale2x(pygame.image.load(os.path.join('imgs/para_frente', 'imagem_3.png'))),
+    pygame.transform.scale2x(pygame.image.load(os.path.join('imgs/para_frente', 'imagem_4.png'))),
+    pygame.transform.scale2x(pygame.image.load(os.path.join('imgs/para_frente', 'imagem_5.png'))),
+    pygame.transform.scale2x(pygame.image.load(os.path.join('imgs/para_frente', 'imagem_6.png'))),
+]
+IMAGENS_SALTO = []
 
 pygame.display.set_caption("Street Fighter")
 screen = pygame.display.set_mode((screen_width, screen_height))
@@ -25,14 +34,14 @@ class Hadouken:
         self.imagem = pygame.image.load(os.path.join('imgs','hadouken.jpg'))
         self.x = Character.x
         self.y = Character.y
-        self.speed = 0.7
+        self.speed = 0.5
     def mover(self):
         self.x += self.speed
     
 
 class Character:
     def __init__(self, x, y): 
-        self.imagem = IMAGENS_PARADO[0]
+        self.imagem = IMAGENS_PARA_TRAS[0]
         self.rect = self.imagem.get_rect()
         self.x = x
         self.y = y
@@ -84,6 +93,7 @@ background_rect.topleft = (0,0)
 
 speed = 1
 tempo = 0
+tempo_parado = 0
 
 while True:
     for event in pygame.event.get():
@@ -91,18 +101,57 @@ while True:
             pygame.quit()
             sys.exit()
 
+    delta = 150
     
     
     keys = pygame.key.get_pressed()
     
     if keys[pygame.K_LEFT]:
+        if tempo == 0*delta:
+            ryu.imagem = IMAGENS_PARA_TRAS[0]
+        if tempo == 1*delta:
+            ryu.imagem = IMAGENS_PARA_TRAS[1]
+        if tempo == 2*delta:
+            ryu.imagem = IMAGENS_PARA_TRAS[2]
+        if tempo == 3*delta:
+            ryu.imagem = IMAGENS_PARA_TRAS[3]
+        if tempo == 4*delta:
+            ryu.imagem = IMAGENS_PARA_TRAS[4]
+        if tempo == 5*delta:
+            ryu.imagem = IMAGENS_PARA_TRAS[5]
+            tempo = -1
+        tempo += 1
         ryu.rect.x -= speed
     if keys[pygame.K_RIGHT]:
+        if tempo == 0*delta:
+            ryu.imagem = IMAGENS_PARA_FRENTE[0]
+        if tempo == 1*delta:
+            ryu.imagem = IMAGENS_PARA_FRENTE[1]
+        if tempo == 2*delta:
+            ryu.imagem = IMAGENS_PARA_FRENTE[2]
+        if tempo == 3*delta:
+            ryu.imagem = IMAGENS_PARA_FRENTE[3]
+        if tempo == 4*delta:
+            ryu.imagem = IMAGENS_PARA_FRENTE[4]
+        if tempo == 5*delta:
+            ryu.imagem = IMAGENS_PARA_FRENTE[5]
+            tempo = -1
+        tempo += 1
         ryu.rect.x += speed
+        
     if keys[pygame.K_UP]:
         ryu.pular()
     if keys[pygame.K_DOWN]:
         ryu.rect.y += speed
+        
+    if not any(keys):
+        if tempo_parado == 0*delta:
+                ryu.imagem = IMAGENS_PARA_FRENTE[0]
+        elif tempo_parado == 3*delta:
+                ryu.imagem = IMAGENS_PARA_FRENTE[5]
+        elif tempo_parado == 6* delta:
+                tempo_parado = -1
+        tempo_parado += 1
     
     ryu.gravidade()    
     ryu.mover()
@@ -119,24 +168,6 @@ while True:
         ryu.rect.y = screen_height - ground.rect.height - ryu.rect.height
         
     #animação dele parado
-    
-    delta = 150
-    
-    if tempo == 0*delta:
-        ryu.imagem = IMAGENS_PARADO[0]
-    if tempo == 1*delta:
-        ryu.imagem = IMAGENS_PARADO[1]
-    if tempo == 2*delta:
-        ryu.imagem = IMAGENS_PARADO[2]
-    if tempo == 3*delta:
-        ryu.imagem = IMAGENS_PARADO[3]
-    if tempo == 4*delta:
-        ryu.imagem = IMAGENS_PARADO[4]
-    if tempo == 5*delta:
-        ryu.imagem = IMAGENS_PARADO[5]
-        tempo = -1
-    
-    tempo += 1
 
     screen.fill(WHITE)
     ground.draw(screen)  # Desenha o chão
